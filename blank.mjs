9881ad070ab0216a25477ff0081c3d34c80ed86f
Jeffrey Alan Scudder <me@jas.life>
2024-01-30 23:47:02 -0400

    easier template

--- blank.mjs
+++ blank.mjs
@@ -1,34 +1,22 @@
 // Blank, 24.01.29.20.07 
 // A blank aesthetic.computer piece. 
 
-/* #region ⭐️ Setup
-  - Install the `aesthetic.computer` extension to the left.
-  - Tap the new 🌀 Spiral icon and follow the instructions in the panel.
-*/
-
 /* #region 📚 README 
-  - Welcome to your first aesthetic.computer piece.
-  - You can `console.log(api);` to explore,
-    and then destructure additional api commands
-    to try them out!
-  - Top-level commands make up each piece's application flow.
-    Uncomment and export them to invoke their behavior and use their apis.
-  - Enter `help` for the AC #help and ping @helper for additional explanation.
-#endregion */
+  1.) Tap 🪟 Extensions icon and install the `aesthetic.computer` extension.
+  2.) Tap the added 🌀 Spiral icon to open the `prompt`.
+  3.) Code below, saving the file to run the piece.
 
-/* #region 🏁 TODO 
-  - [] Draw a purple line.
+  ❓  Enter `help` for the AC #help and ping @helper for additional explanation.
 #endregion */
 
 function boot({ wipe }) {
-  // Runs once at the start.
-  wipe("blue"); // Clear's the screen. Can use R, G, B or CSS colors.
+  wipe("blue");
 }
 
-function paint({ api, ink, line, pen, box }) {
+// function paint({ ink, line, box, pen }) {
   // ink("red").line(0, 0, 100, 100); // Would draw a diagonal line.
   // return false; // Uncomment for proce55ing's "noLoop" functionality.
-}
+// }
 
 // function act({ event: e }) {
 //  // Respond to user input here.
@@ -61,7 +49,7 @@ function meta() {
 // Render an application icon, aka favicon.
 // }
 
-export { boot, paint, meta };
+export { boot, meta };
 
 // 📚 Library
 //   (Useful functions used throughout the piece)
